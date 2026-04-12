5ddbc25432f2d04c6b477592d802a66ddcd34bf7
Mistivia <i@mistivia.com>
2026-04-12 17:17:01 +0800

    update

--- scripts/typ2html.py
+++ scripts/typ2html.py
@@ -98,7 +98,9 @@ def build_html(svg_names: list[str], title: str, srctext: str = "") -> str:
 <div class="ipadding">
 <pre class="back"><a href="../">../</a></pre>
 </div>
-<pre style="position:absolute;left:-10000px;top:-10000px;opacity:0;width:1px;height:1px;overflow:hidden;">{srctext[:500] + '\n\n...'}</pre>
+<p style="position:absolute;left:-10000px;top:-10000px;opacity:0;width:1px;height:1px;overflow:hidden;">
+    {srctext[:500].replace('\n', '<br>').replace(' ', '&#32;') + '<br><br>...'}
+</p>
 <div class="content">
 {pages_html}
 </div>
